ea1a207ab56a413015b52c3860f84268da4e22fa
Ashutosh <ashutoshsenapati2311@gmail.com>
2025-02-08 18:57:09 +0530

    chore: formatting

--- src/services/user.js
+++ src/services/user.js
@@ -1,15 +1,18 @@
-const user= require('../models').users;
+const user = require('../models').users;
 
 const getAllUsers = async () => {
   const users = await user.findAll();
   return users;
 };
 const createUser = async (name, email, phone, designation, company_id) => {
-  const newUser = await user.create({name, email, phone, designation, company_id});
+  const newUser = await user.create({
+    name,
+    email,
+    phone,
+    designation,
+    company_id,
+  });
   return newUser;
-}
+};
 
-module.exports =
-  {getAllUsers,
-  createUser}
-;
+module.exports = { getAllUsers, createUser };
